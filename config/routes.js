0060b298440d296d6984583970b804d9c481255e
Przemyslaw Krepski <krepski80@gmail.com>
2024-09-27 22:58:20 +0200

    added login, register and home views with styles

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,9 @@
+const authRoutes = require('../routes/authRoutes');
 const chatRoutes = require('../routes/chatRoutes');
 const gameRoutes = require('../routes/gameRoutes');
 
 module.exports = (app) => {
-    app.use('/', chatRoutes);  // Chat routes
+    app.use('/', authRoutes);  // Auth and home routes
+    app.use('/chat', chatRoutes);  // Chat routes
     app.use('/game', gameRoutes);  // Game routes
 };
